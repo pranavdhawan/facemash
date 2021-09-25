6bef313ccdba385b3d5b52ad42f439519069bb79
Siddharth Nikhil <mailtosiddnikh@gmail.com>
2021-09-25 18:30:42 +0530

    FUCK YES

--- helper.py
+++ helper.py
@@ -1,1 +1,13 @@
 #Helper functions we don't want in main file to keep it clean.
+
+#function to get followers
+import instaloader
+from instaloader import Profile
+def followers_list(username):
+    L = instaloader.Instaloader()
+    L.login("pranavdhawan4", "itsanewpassword")
+
+    profile = Profile.from_username(L.context, username)
+    print(f"{username} is followed by these profiles: ")
+    for follower in profile.get_followers():
+        print(follower.username)
