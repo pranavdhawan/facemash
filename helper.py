f8cac5c1ee4627747634ddbc2e1995223aa7de97
Siddharth Nikhil <mailtosiddnikh@gmail.com>
2021-09-25 19:09:49 +0530

    url milgao jaani

--- helper.py
+++ helper.py
@@ -11,3 +11,10 @@ def followers_list(username):
     print(f"{username} is followed by these profiles: ")
     for follower in profile.get_followers():
         print(follower.username)
+
+def get_dp_url(username):
+    L = instaloader.Instaloader()
+    L.login("pranavdhawan4", "itsanewpassword")
+
+    profile = Profile.from_username(L.context, username)
+    print(profile.profile_pic_url)
