a592ab5f7bac020a7e1edac8cfa827928782d9ff
Pranav Dhawan <dhawan.pranav8@gmail.com>
2021-09-25 21:12:58 +0530

    svqwfw

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #main flask file.
+from urllib.request import urlopen
 
 from flask import Flask, render_template, request, session
 import helper as hp
@@ -7,6 +8,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    with urlopen('https://api.instagram.com/v1/users/pranavdhawan4/?accesstoken=ACCESS-TOKEN') as r:
+        img = r.read()
     return render_template("layout.html")
 
 if __name__ == "__main__":
