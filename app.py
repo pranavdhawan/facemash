370536a80966d7fd72f230067335dbf8fe1bedf1
Pranav Dhawan <dhawan.pranav8@gmail.com>
2021-09-25 18:08:15 +0530

    wkfw

--- app.py
+++ app.py
@@ -2,3 +2,12 @@
 
 from flask import Flask, render_template, request, session
 import helper as hp
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("layout.html")
+
+if __name__ == "__main__":
+	app.run(debug = True)
